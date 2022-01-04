6c1cdf7259bdedb573bc46c299c88fa48baac1ef
Ali Raza <70605579+alirazamumtaz@users.noreply.github.com>
2022-01-04 23:54:00 +0500

    Project 3 has been created

---
+++ RockPaperScissor/RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Ali Raza on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
