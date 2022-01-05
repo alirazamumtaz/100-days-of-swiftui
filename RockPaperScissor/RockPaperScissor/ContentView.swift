e298935b4a80774f9e09de7c6ebd9860b7068a09
Ali Raza <70605579+alirazamumtaz@users.noreply.github.com>
2022-01-06 01:42:21 +0500

    Logic has been written

--- RockPaperScissor/RockPaperScissor/ContentView.swift
+++ RockPaperScissor/RockPaperScissor/ContentView.swift
@@ -14,10 +14,13 @@ struct ContentView: View {
 //                .font(.headline)
 //        }
 //    }
+    
+    
+    
     @State private var options = [["rock","paper","scissors"],
                                   ["👊🏻"  ,"✋🏻"   ,"✌🏻"]]
     @State private var wins = [1,2,0]
-    @State private var notWin = [2,0,1]
+    @State private var loses = [2,0,1]
     @State private var computersOption = 0
     @State private var toWin = Bool.random()
     @State private var usersOption = 0
@@ -26,75 +29,117 @@ struct ContentView: View {
     @State private var draws = 0
     let rounds = 10
     var body: some View {
-        NavigationView {
-            VStack {
+        VStack {
+            Spacer()
+            HStack {
                 Spacer()
-                HStack {
-                    Spacer()
-                    VStack {
-                        Text("User's Score")
-                            .font(.headline)
-                        Text(String(userScore))
-                            .font(.caption)
-                    }
-                    Spacer()
-                    VStack {
-                        Text("Draws")
-                            .font(.headline)
-                        Text(String(draws))
-                            .font(.caption)
-                    }
-                    Spacer()
-                    VStack {
-                        Text("User's Score")
-                            .font(.headline)
-                        Text(String(userScore))
-                            .font(.caption)
-                    }
-                    Spacer()
+                VStack {
+                    Text("User's Score")
+                        .font(.headline)
+                    Text(String(userScore))
+                        .font(.caption)
                 }
                 Spacer()
-                Text("Computer Option")
-                    .font(.headline)
-                Text(options[1][computersOption])
-                    .font(.system(size:100))
-                Text(options[0][computersOption])
-                        .textCase(.uppercase)
+                VStack {
+                    Text("Draws")
+                        .font(.headline)
+                    Text(String(draws))
+                        .font(.caption)
+                }
                 Spacer()
-                HStack {
-                    ForEach(0..<3){ i in
-                        VStack {
-                            Button(options[1][i]) {
-                                usersOption = i
-                                computersOption = Int.random(in: 0..<3)
-                                print(usersOption, " ", computersOption)
-//                                if toWin && usersOption == wins[usersOption] {
-//                                    userScore += 1
-//                                }
-//                                else if !toWin && usersOption == notWin[usersOption] {
-//                                    userScore += 1
-//                                }
-//                                else if toWin && usersOption != wins[usersOption] {
-//                                    computerScore += 1
-//                                }
-//                                else if !toWin && usersOption != notWin[usersOption] {
-//                                    computerScore += 1
-//                                }
-                                if usersOption == (computerScore+1)%3{
-                                    
-                                }
-                                else {
-                                    draws += 1
-                                }
-                            }
-                            .font(.system(size:100))
-                            Text(options[0][i])
-                                .textCase(.uppercase)
-                        }
-                    }
+                VStack {
+                    Text("Computere's Score")
+                        .font(.headline)
+                    Text(String(computerScore))
+                        .font(.caption)
                 }
                 Spacer()
             }
+            Spacer()
+            Spacer()
+            // Showing Options
+            HStack{
+                Spacer()
+                VStack {
+                    Text("User's Option")
+                        .font(.headline)
+                    Text(options[1][usersOption])
+                        .font(.system(size:100))
+                        .transition(.scale)
+                    Text(options[0][usersOption])
+                            .textCase(.uppercase)
+                }
+                Spacer()
+                VStack {
+                    Text("Computer Option")
+                        .font(.headline)
+                    Text(options[1][computersOption])
+                        .font(.system(size:100))
+                        .transition(.scale)
+                    Text(options[0][computersOption])
+                            .textCase(.uppercase)
+                }
+                Spacer()
+            }
+    
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.secondary)
+                    .frame(width: 300, height: 200, alignment: .center)
+                    .padding()
+                VStack {
+                    Text("Choose your MOVE")
+                    HStack {
+                        ForEach(0..<3){ i in
+                            VStack {
+                                Button(options[1][i]) {
+                                    usersOption = i
+                                    withAnimation {
+                                        computersOption = Int.random(in: 0..<3)
+                                    }
+                                    if usersOption == computersOption{
+                                        // print("Both players selected ", userScore, ". It's a tie!")
+                                        draws += 1
+                                    }
+                                    else if usersOption == 0 {
+                                        if computersOption == 2 {
+    //                                        print("Rock smashes scissors! You win!")
+                                            userScore += 1
+                                        } else {
+    //                                        print("Paper covers rock! You lose.")
+                                            computerScore += 1
+                                        }
+                                    }
+                                    else if usersOption == 1 {
+                                        if computersOption == 0 {
+    //                                        print("Paper covers rock! You win!")
+                                            userScore += 1
+                                        } else {
+    //                                        print("Scissors cuts paper! You lose.")
+                                            computerScore += 1
+                                        }
+                                    }
+                                    else if usersOption == 2 {
+                                        if computersOption == 1 {
+    //                                        print("Scissors cuts paper! You win!")
+                                            userScore += 1
+                                        } else {
+    //                                        print("Rock smashes scissors! You lose.")
+                                            computerScore += 1
+                                        }
+                                    }
+                                }
+                                .padding()
+                                .font(.system(size:50))
+                                Text(options[0][i])
+                                    .textCase(.uppercase)
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer()
         }
     }
 }
